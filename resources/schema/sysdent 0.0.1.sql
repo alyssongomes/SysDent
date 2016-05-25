7d1b784ce2fe80bae9c5d25d22c42a524b69aab1
Allan Vidal <allanfvc@gmail.com>
2016-05-25 08:28:32 -0300

    Alteração do esquema do banco

--- resources/schema/sysdent 0.0.1.sql
+++ resources/schema/sysdent 0.0.1.sql
@@ -3,16 +3,6 @@
 
 
 
-CREATE TABLE user (
-cpf int PRIMARY KEY,
-name varchar(50),
-street varchar(50),
-number varchar(50),
-district varchar(50),
-zipcode int,
-idRole int
-)
-
 CREATE TABLE role (
 id int PRIMARY KEY,
 role varchar(50)
@@ -23,18 +13,26 @@ id int PRIMARY KEY,
 idPatient int,
 idDentist int,
 date date,
-time time,
-FOREIGN KEY(idPatient) REFERENCES user (cpf),
-FOREIGN KEY(idDentist) REFERENCES user (cpf)
+time time
 )
 
 CREATE TABLE diagnostic (
 id int PRIMARY KEY,
 idPatient int,
 idDentist int,
-diagnostic varchar(500),
-FOREIGN KEY(idPatient) REFERENCES user (cpf),
-FOREIGN KEY(idDentist) REFERENCES user (cpf)
+diagnostic varchar(500)
+)
+
+CREATE TABLE user (
+cpf int PRIMARY KEY,
+name varchar(50),
+street varchar(50),
+number varchar(50),
+district varchar(50),
+zipcode int,
+idRole int,
+password varchar(50),
+FOREIGN KEY(idRole) REFERENCES role (id)
 )
 
 CREATE TABLE payment (
@@ -45,4 +43,7 @@ date date,
 FOREIGN KEY(idPatient) REFERENCES user (cpf)
 )
 
-ALTER TABLE user ADD FOREIGN KEY(idRole) REFERENCES role (id)
+ALTER TABLE appointment ADD FOREIGN KEY(idPatient) REFERENCES user (cpf)
+ALTER TABLE appointment ADD FOREIGN KEY(idDentist) REFERENCES user (cpf)
+ALTER TABLE diagnostic ADD FOREIGN KEY(idPatient) REFERENCES user (cpf)
+ALTER TABLE diagnostic ADD FOREIGN KEY(idDentist) REFERENCES user (cpf)
