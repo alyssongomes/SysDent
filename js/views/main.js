348b39a9b7dc4d0ec64fbf3879d0709dd4dab552
Allan Vidal <allanfvc@gmail.com>
2016-06-10 16:35:24 -0300

    Ajuste no Calendário

--- js/views/main.js
+++ js/views/main.js
@@ -1,6 +1,22 @@
 window.onload = function(){
   $("#calendar").datepicker({
-      language: "pt-BR",
-      todayHighlight: true
+    weekStart: 1,
+    maxViewMode: 2,
+    todayBtn: "linked",
+    language: "pt-BR",
+    daysOfWeekDisabled: "0",
+    daysOfWeekHighlighted: "0",
+    todayHighlight: true
   });
+
+  $('#inCalendar input').datepicker({
+    autoclose: true,
+    weekStart: 1,
+    maxViewMode: 2,
+    todayBtn: "linked",
+    language: "pt-BR",
+    daysOfWeekDisabled: "0",
+    daysOfWeekHighlighted: "0",
+    todayHighlight: true
+});
 }
